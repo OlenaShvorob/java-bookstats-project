892635cdf641217f8a8241cdeaea1a20c67d1ff7
OlenaShvorob <shvoroblena159@gmail.com>
2025-11-21 19:28:16 +0200

    Initial commit

---
+++ src/main/java/ua/olena/bookstats/model/Book.java
@@ -0,0 +1,50 @@
+package ua.olena.bookstats.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int yearPublished, String genre) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
